c0def07067bf91fa24b77ffa9333dcfb8086ed8f
reimiii <imiia75775@gmail.com>
2023-07-24 19:57:03 +0700

    create database.sql

---
+++ database.sql
@@ -0,0 +1,16 @@
+show databases;
+
+create database java_todolist;
+
+use java_todolist;
+
+create table todolist
+(
+    id   int          not null auto_increment,
+    todo varchar(225) not null,
+    primary key (id)
+) engine = innodb;
+
+show tables;
+
+desc todolist;
